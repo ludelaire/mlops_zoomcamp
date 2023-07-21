6b8645647b274f48b77a9db3d895ed59b168598e
Luisa Alarcon <luisaalarcons30@gmail.com>
2023-07-20 23:04:59 -0500

    initial setup of module 6, halfway through 4.4

--- 4.Deployment/streaming/lambda_function.py
+++ 4.Deployment/streaming/lambda_function.py
@@ -9,15 +9,15 @@ kinesis_client = boto3.client('kinesis')
 
 PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'ride_predictions')
 
+RUN_ID = '682824de8ca349bc908f40268a891c34'
+EXPERIMENT_VER = 2
 
-RUN_ID = os.getenv('RUN_ID')
-
-logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
+logged_model = f's3://mlops-zoomcamp-taxi-data/models/{EXPERIMENT_VER}/{RUN_ID}/artifacts/model'
 # logged_model = f'runs:/{RUN_ID}/model'
 model = mlflow.pyfunc.load_model(logged_model)
 
-
 TEST_RUN = os.getenv('TEST_RUN', 'False') == 'True'
+
 
 def prepare_features(ride):
     features = {}
